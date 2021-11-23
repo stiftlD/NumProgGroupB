02606eb0bbd02bc85d0e2c934b60178dbbe624c8
David Stiftl <david.stiftl@tum.de>
2021-11-23 20:59:28 +0100

    ifft

--- PA2/dft/IFFT.java
+++ PA2/dft/IFFT.java
@@ -1,6 +1,7 @@
 package dft;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * Schnelle inverse Fourier-Transformation
@@ -16,6 +17,37 @@ public class IFFT {
      */
     public static Complex[] ifft(Complex[] c) {
         // TODO: diese Methode ist zu implementieren
-        return null;
+        
+        int n = c.length;
+        Complex[] v = new Complex[n];
+
+        if (n==1){
+            return new Complex[] {c[0]};
+        }
+        else{
+            int m = n / 2; //n power of 2
+            //divide
+            ArrayList<Complex> c1 = new ArrayList<Complex>(); //even indices of c
+            ArrayList<Complex> c2 = new ArrayList<Complex>(); //odd indices of c
+            for(int i = 0; i < n; i++){
+                if (i % 2 == 0) c1.add(c[i]);
+                else c2.add(c[i]);               
+            }
+
+            Complex[] z1 = ifft((Complex[]) c1.toArray());
+            Complex[] z2 = ifft((Complex[]) c2.toArray());
+
+            //berechne omega in Polarkoordinaten
+            Complex omega = Complex.fromPolar(1, (2 * Math.PI)/n);
+            
+            //and conquer
+            for (int j = 0; j < m; j++){
+                    v[j] = z1[j].add((omega.power(j).mul(z2[j]))); // v[j] = z1[j] + omega^j * z2[j]
+                    v[m+j] = z1[j].sub((omega.power(j).mul(z2[j]))); // v[m+j] = z1[j] - omega^j * z2[j]
+            }
+
+        }
+
+        return v;
     }
 }
