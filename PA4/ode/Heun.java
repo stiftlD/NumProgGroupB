de5cbfb0585da185cf18ce2b7c4805c87659797b
David Stiftl <david.stiftl@tum.de>
2022-01-13 17:53:22 +0100

    Heun

--- PA4/ode/Heun.java
+++ PA4/ode/Heun.java
@@ -1,4 +1,5 @@
 package ode;
+import java.util.Arrays;
 
 /**
  * Das Einschrittverfahren von Heun
@@ -15,7 +16,28 @@ public class Heun implements Einschrittverfahren {
      */
     public double[] nextStep(double[] y_k, double t, double delta_t, ODE ode) {
         // TODO: diese Methode ist zu implementieren
-        return null;
+        //first step: _r(t+d_t) = r(t) + d_t*f(t, r(t))
+        //second step: r(t+d_t) = r(t) + (1/2) * d_t*(f(t,r(t)) + f(t+d_t, _r(t+d_t)))
+
+
+        //1. step
+        double[] _r = y_k.clone();
+
+        double[] f1 = ode.auswerten(t, y_k);
+
+        for (int i = 0; i < _r.length; i++){
+            _r[i] += delta_t*f1[i];
+        }
+
+        //2. step
+        double[] result = y_k.clone();
+        double[] f2 = ode.auswerten(t + delta_t, _r);
+
+        for (int i = 0; i < result.length; i++){
+            result[i] += (delta_t / 2) * (f1[i] + f2[i]);
+        }
+
+        return result;
     }
 
 }
