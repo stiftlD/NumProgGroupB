21b19c752e6330f14c8205b2fe2097dfa485af81
David Stiftl <david.stiftl@tum.de>
2022-01-13 15:32:03 +0100

    expl euler

--- PA4/ode/ExpliziterEuler.java
+++ PA4/ode/ExpliziterEuler.java
@@ -1,6 +1,7 @@
 package ode;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * Das Einschrittverfahren "Expliziter Euler"
@@ -12,7 +13,18 @@ public class ExpliziterEuler implements Einschrittverfahren {
 
     public double[] nextStep(double[] y_k, double t, double delta_t, ODE ode) {
         // TODO: diese Methode ist zu implementieren
-        return null;
+        //r(t + d_t) = r(t) + d_t*f(t,r(t))
+        //f: velocity at time t at position r(t)
+        //v(t) = v(0) + (0,-g)*t
+
+        double[] result = y_k;
+
+        double[] f = ode.auswerten(t, y_k);
+        for (int i = 0; i < f.length; i++){
+            result[i] += delta_t*f[i];
+        }
+
+        return result;
     }
 
 }
